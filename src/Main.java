75ea6a3917842138f294c51046561d36333510ed
dmitriyalpatov <dmitriyalpatov@mail.ru>
2022-05-22 20:06:09 +0300

    commit1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+                int eaters = 5; // how many people will eat
+
+                int water = 3000; // milliliters of water
+                int potatoes = 5; // potatoes pcs
+                int chicken = 6; // chicken thighs
+                int spices = 10; // spoons of spices
+
+                System.out.println("The soup was cooked. For one person it turned out:");
+                System.out.println((water / eaters) + " ml vater");
+                System.out.println((potatoes / eaters) + " potato pc )");
+                System.out.println((chicken / eaters) + " chicken thighs)");
+                System.out.println((spices / eaters) + " spoons of spices");
+
+
+
+    }
+}
